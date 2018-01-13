2cd971aa659a15678c09953e04346919119acd0f
madhawap <madhawa.818@gmail.com>
2018-01-13 23:58:02 +0530

    committing initial project content

---
+++ sql.py
@@ -0,0 +1,19 @@
+# sql.py - Create a SQLite3 table and populate it with data
+
+
+import sqlite3
+
+# create a new database if the database doesn't already exist
+with sqlite3.connect('sample.db') as connection:
+
+    # get a cursor object used to execute SQL commands
+    c = connection.cursor()
+
+    # create the table
+    c.execute('CREATE TABLE sampleTable(id TEXT, value TEXT)')
+
+    # insert dummy data into the table
+    c.execute('INSERT INTO sampleTable VALUES("01", "Sample value 01")')
+    c.execute('INSERT INTO sampleTable VALUES("02", "Sample value 02")')
+    c.execute('INSERT INTO sampleTable VALUES("03", "Sample value 03")')
+    c.execute('INSERT INTO sampleTable VALUES("04", "Sample value 04")')
